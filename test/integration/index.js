620a17e3d8daf048e186fa7316badfa99dbc68eb
Jong Kim <jkim@twilio.com>
2018-08-06 12:53:18 -0700

    Adding Justin's commits to stabilize tests

--- test/integration/index.js
+++ test/integration/index.js
@@ -1,9 +1,6 @@
 require('./spec/Activity');
 require('./spec/Channel');
-require('./spec/ReservationAccept');
-require('./spec/ReservationReject');
 require('./spec/Supervisor');
 require('./spec/WorkerCommon');
 require('./spec/WorkerMultiTask');
 require('./spec/WorkerNonMultiTask');
-require('./spec/TaskEvents');
