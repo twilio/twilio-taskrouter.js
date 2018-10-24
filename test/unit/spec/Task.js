bc48e05e7a6d6e49cf78c0a09ac7c833c018ee38
Alexander Laird <alaird@twilio.com>
2018-10-24 16:12:30 -0700

    Adding "region" parameter to Worker.js for event-bridge Interconnect.

--- test/unit/spec/Task.js
+++ test/unit/spec/Task.js
@@ -386,7 +386,7 @@ describe('Task', () => {
                 languages: ['en']
             },
             Mode: 'cold',
-            Priority: undefined,
+            Priority: 'undefined',
             ReservationSid: reservationSid,
             To: 'alice',
         };
