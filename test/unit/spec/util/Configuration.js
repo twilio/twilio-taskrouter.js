59ddda42fd0ffeaf5c2e2233f2e42e7768cd7c9f
Alexander Laird <alaird@twilio.com>
2018-10-25 12:03:17 -0700

    Fixing tests, clarifying region parameter splicing in FQDN for EB.

--- test/unit/spec/util/Configuration.js
+++ test/unit/spec/util/Configuration.js
@@ -25,7 +25,7 @@ describe('Configuration', () => {
       assert.equal(config.WS_SERVER, 'wss://event-bridge.dev-us1.twilio.com/v1/wschannels');
     });
 
-    it('region should override ebServer option if passed', () => {
+    it('region should append to ebServer', () => {
         const options = {
             ebServer: 'https://event-bridge.dev-us1.twilio.com/v1/wschannels',
             wsServer: 'wss://event-bridge.dev-us1.twilio.com/v1/wschannels',
@@ -34,8 +34,8 @@ describe('Configuration', () => {
 
         const config = new Configuration(token, options);
 
-        assert.equal(config.EB_SERVER, 'https://event-bridge.ie1-ix.us1.twilio.com/v1/wschannels');
-        assert.equal(config.WS_SERVER, 'wss://event-bridge.ie1-ix.us1.twilio.com/v1/wschannels');
+        assert.equal(config.EB_SERVER, 'https://event-bridge.dev-us1.ie1-ix.us1.twilio.com/v1/wschannels');
+        assert.equal(config.WS_SERVER, 'wss://event-bridge.dev-us1.ie1-ix.us1.twilio.com/v1/wschannels');
     });
 
     it('should use environment default if options not provided', () => {
