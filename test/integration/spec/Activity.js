211c963aa6c9310efc458971b70d9a718e55f278
Claire Huang <clhuang@twilio.com>
2019-02-15 13:58:41 -0800

    broken test

--- test/integration/spec/Activity.js
+++ test/integration/spec/Activity.js
@@ -1,5 +1,6 @@
 import EnvTwilio from '../../util/EnvTwilio';
 import Worker from '../../../lib/Worker';
+import { doesNotReject } from 'assert';
 
 const chai = require('chai');
 const expect = chai.expect;
@@ -29,37 +30,94 @@ describe('Activity', () => {
         });
     });
 
-    describe('#setAsCurrent', () => {
-        it('should set this connect activity on the Worker, and then update it', () => {
-            worker = new Worker(token, {
+    // describe('#setAsCurrent', () => {
+    //     it('should set this connect activity on the Worker, and then update it', () => {
+    //         worker = new Worker(token, {
+    //             ebServer: `${credentials.ebServer}/v1/wschannels`,
+    //             wsServer: `${credentials.wsServer}/v1/wschannels`,
+    //             connectActivitySid: credentials.nonMultiTaskConnectActivitySid
+    //         });
+
+    //         let connectActivity;
+    //         let updateActivity;
+    //         return worker.on('activityUpdated', async connectWorker => {
+    //             assert.isNotNull(worker.activities);
+    //             assert.equal(worker.activities.size, 4);
+
+    //                 worker.activities.forEach(activity => {
+    //                     if (activity.sid === credentials.nonMultiTaskConnectActivitySid) {
+    //                         connectActivity = activity;
+    //                     }
+    //                     if (activity.sid === credentials.nonMultiTaskUpdateActivitySid) {
+    //                         updateActivity = activity;
+    //                     }
+    //                 });
+
+    //                 expect(worker.activity).to.deep.equal(connectWorker.activity);
+
+    //             const updatedActivity = await updateActivity.setAsCurrent();
+    //             expect(worker.activity).to.deep.equal(updateActivity);
+    //             expect(worker.activity).to.deep.equal(updatedActivity);
+    //             assert.isTrue(updateActivity.isCurrent);
+    //             assert.isFalse(connectActivity.isCurrent);
+    //         });
+    //     }).timeout(5000);
+    // });
+
+    describe('#setRejectPendingReservationsFlag', () => {
+        it('should set reject the pending reservations for the Worker when the flag is set to true, and update the activity', () => {
+            worker = new Worker(token,  {
                 ebServer: `${credentials.ebServer}/v1/wschannels`,
                 wsServer: `${credentials.wsServer}/v1/wschannels`,
                 connectActivitySid: credentials.nonMultiTaskConnectActivitySid
             });
-
             let connectActivity;
             let updateActivity;
-            return worker.on('activityUpdated', async connectWorker => {
-                assert.isNotNull(worker.activities);
-                assert.equal(worker.activities.size, 4);
+            let reservations;
 
-                    worker.activities.forEach(activity => {
-                        if (activity.sid === credentials.nonMultiTaskConnectActivitySid) {
-                            connectActivity = activity;
-                        }
-                        if (activity.sid === credentials.nonMultiTaskUpdateActivitySid) {
-                            updateActivity = activity;
-                        }
-                    });
+            return new Promise(resolve =>  {
+                worker.on('reservationCreated', reservation => {
+                    reservations.push(reservation)
+                    if(worker.reservations.size = 2) {
+                        return resolve(reservations)
+                    }
+                });
+            }).then(async reservations => {
+                // assert.equal(worker.reservation.size, 2);
+                // reservations.forEach(reservation => {
+                //     assert.equal(reservation.status, 'pending');
+                //     assert.equal(reservation.sid.substring(0, 2), 'WR');
+                //     assert.equal(reservation.task.sid.substring(0, 2), 'WT');
+                //     assert.equal(reservation.task.taskChannelUniqueName, 'default');
+                // })
+                // worker.activities.forEach(activity => {
+                //     if (activity.sid === credentials.nonMultiTaskConnectActivitySid) {
+                //         connectActivity = activity;
+                //     }
+                //     if (activity.sid === credentials.nonMultiTaskUpdateActivitySid) {
+                //         updateActivity = activity;
+                //     }
+                // });
 
-                    expect(worker.activity).to.deep.equal(connectWorker.activity);
+                // expect(worker.activity).to.deep.equal(connectWorker.activity);
 
-                const updatedActivity = await updateActivity.setAsCurrent();
-                expect(worker.activity).to.deep.equal(updateActivity);
-                expect(worker.activity).to.deep.equal(updatedActivity);
-                assert.isTrue(updateActivity.isCurrent);
-                assert.isFalse(connectActivity.isCurrent);
-            });
+                // const updatedActivity = await updateActivity.setAsCurrent(true);
+                // expect(worker.activity).to.deep.equal(updateActivity);
+                // expect(worker.activity).to.deep.equal(updatedActivity);
+                // assert.isTrue(updateActivity.isCurrent);
+                // assert.isFalse(connectActivity.isCurrent);
+
+                // reservations.forEach(reservation => {
+                //     expect(reservation.status).equal('rejected');
+                
+                // });
+                done();
+
+                return reservations;
+            })
+
         }).timeout(5000);
-    });
+    })
+
+
 });
