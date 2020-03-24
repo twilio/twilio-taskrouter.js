48489f8013f6267f26619fe2be6ed6a991f6af5a
Wanjun Li <wli@twilio.com>
2020-03-24 16:28:26 -0700

    ORCH-455: Add test cases for Outbound Supervisor (#123)
    * Add test cases for Outbound Supervisor

--- test/voice/index.js
+++ test/voice/index.js
@@ -1,7 +1,8 @@
 require('./inbound/spec/Reservation');
 require('./inbound/spec/Supervisor');
 require('./outbound/spec/Reservation');
-require('./outbound/spec/TaskTransferToWorker');
+require('./outbound/spec/Supervisor');
 require('./outbound/spec/Task');
 require('./outbound/spec/TaskTransferToQueue');
+require('./outbound/spec/TaskTransferToWorker');
 
