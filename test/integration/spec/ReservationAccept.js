269f0db4dba0516afb51df69273ee94bbf0353f9
Yasharth Ji Bhatt <ybhatt@twilio.com>
2022-06-15 19:59:56 +0530

    changes done to avoid no-trailing-spaces related error

--- test/integration/spec/ReservationAccept.js
+++ test/integration/spec/ReservationAccept.js
@@ -195,6 +195,7 @@ describe('Reservation Accept', () => {
                                     .then(results => {
                                         assert.equal(results.length, 2,
                                             envTwilio.getErrorMessage('Completed results count mismatch', credentials.accountSid, credentials.multiTaskConnectActivitySid));
+
                                         let reservation = results[0];
                                         expect(reservation.task.reason).to.equal('Work is done');
                                         expect(reservation.task.status).to.equal('completed');
@@ -202,4 +203,4 @@ describe('Reservation Accept', () => {
                                     });
         }).timeout(5000);
     });
-}); 
+});
