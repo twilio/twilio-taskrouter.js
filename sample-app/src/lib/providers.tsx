91c33d19ce1f8002a2340f72bbf38abc811f4a88
kirilxd <kriazantsev@twilio.com>
2025-10-10 10:57:48 +0000

    initial commit

--- sample-app/src/lib/providers.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import React from 'react';
-import { LogContextProvider } from './log-context';
-
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <LogContextProvider>{children}</LogContextProvider>;
-}
