17121f4f91762e18b13f9be70a17ddee047e57bb
flexrouting+ci <flexrouting+ci@twilio.com>
2026-02-26 13:23:26 +0000

    3.0.0-rc.1

--- test/mocha/no-unicode-reporter.js
+++
@@ -1,4 +0,0 @@
-import mocha from 'mocha';
-
-mocha.reporters.Base.symbols.ok = '[PASS]';
-mocha.reporters.Base.symbols.err = '[FAIL]';
--- test/util/Constants.js
+++
@@ -1,7 +0,0 @@
-// Cancel Outbound Reservation
-export const RESERVATION_CANCELED_REASON = 13223;
-export const CONNECTING_PARTICIPANT_ERROR_CODE = 21215;
-export const INVALID_NUMBER = '+1234567891011';
-export const GEO_NOT_ALLOW_NUMBER = '+8836231420287'; // an invalid country code number
-export const TASK_CANCELED_REASON = 'RoutingTarget not available for Reservation acceptance: ';
-export const TRANSFER_MODE = Object.freeze({ cold: 'COLD', warm: 'WARM' });
